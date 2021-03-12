a1ecb698cce221b04a17c69258566126b00829ab
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-03-12 14:38:50 -0600

    No commit message

--- jquery.magnify-mobile.js
+++ jquery.magnify-mobile.js
@@ -1,6 +1,8 @@
-if("ontouchstart" in window) {
-  var link = document.createElement("a");
-  link.href = "101 Prayer Questions.svg";
-  document.body.appendChild(link);
-  link.click();
-}
+document.addEventListener("DOMContentLoaded", () => {
+  if("ontouchstart" in window) {
+    var link = document.createElement("a");
+    link.href = "101 Prayer Questions.svg";
+    document.body.appendChild(link);
+    link.click();
+  }
+});
